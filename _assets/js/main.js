adf02f60c105ae041e8c9f4b7c0a69c906a35bc6
jtournay <jtournay@linkedin.com>
2013-05-05 18:30:06 +0200

    first page animation

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -7,4 +7,53 @@
  */
 $(function(){
   $(document.body).addClass('ready');
+
+  var anims =
+    [
+      {
+        el: $('.hi > *'),
+        props: {
+          'opacity': [1, -1.4],
+          'top': [0, -400, 'px']
+        }
+      },
+      {
+        el: $('.hi > h1 em'),
+        props: {
+          'top': [0, -450, 'px']
+        }
+      },
+      {
+        el: $('.hi > h2'),
+        props: {
+          'top': [0, -300, 'px']
+        }
+      }
+    ]
+
+  $(window).scroll(function(evt){
+    var height = $(window).height(),
+        scroll = $(window).scrollTop(),
+        progress = (scroll / height) // % of page scrolled
+
+    $(anims).each(function(i, a){
+
+      var styles =
+        _.reduce(_.pairs(a.props), function(ps, p){
+          var name = p[0],
+              vs = p[1],
+              current = (vs[1] - vs[0]) * progress + vs[0] + (vs[2] | '');
+
+            console.log("%o => %o", name, (vs[1] - vs[0]) * progress)
+
+          var x = {}
+          x[name] = current
+          return _.extend({}, ps, x)
+        }, {})
+
+      a.el.css(styles)
+
+    })
+
+  })
 })
