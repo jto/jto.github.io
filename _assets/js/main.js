6ff00daa4555ae672e06c3cacbd686bb5a014721
jtournay <jtournay@linkedin.com>
2013-05-06 17:22:39 +0200

    buggy cloud

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -13,65 +13,96 @@ $(function(){
       {
         el: $('.hi > *'),
         props: {
-          'opacity': [1, -1.4],
-          'top': [0, -400, 'px']
-        }
+          'opacity': [1, 0],
+          'top': [0, -50, '%']
+        },
+        bounds: [0, .5]
       },
       {
         el: $('.hi > h1 em'),
         props: {
           'top': [0, -450, 'px']
-        }
+        },
+        bounds: [0, 1]
       },
       {
         el: $('.hi > h2'),
         props: {
           'top': [0, -300, 'px']
-        }
+        },
+        bounds: [0, 1]
       },
       {
         el: $('.webapps .cloud.scalable'),
         props: {
-          'left': [-100, 800, '%']
-        }
+          'left': [-40, 37, '%']
+        },
+        bounds: [.3, .4]
+      },
+      {
+        el: $('.webapps .cloud.scalable'),
+        props: {
+          'left': [37, 110, '%']
+        },
+        bounds: [.6, .7]
       },
       {
         el: $('.webapps .cloud.distributed'),
         props: {
-          'left': [-500, 450, '%']
-        }
+          'left': [-40, 37, '%']
+        },
+        bounds: [.7, .8]
+      },
+      {
+        el: $('.webapps .cloud.distributed'),
+        props: {
+          'left': [37, 110, '%']
+        },
+        bounds: [1, 1.1]
       },
       {
         el: $('.webapps .cloud.web'),
         props: {
-          'left': [-800, 50, '%']
-        }
+          'left': [-40, 37, '%']
+        },
+        bounds: [1.1, 1.2]
+      }
+      ,
+      {
+        el: $('.webapps .cloud.web'),
+        props: {
+          'left': [37, 110, '%']
+        },
+        bounds: [1.3, 1.5]
       }
     ]
 
-  $(window).scroll(function(evt){
+  $(window).scroll(_.debounce(function(evt){
     var height = $(window).height(),
         scroll = $(window).scrollTop(),
-        progress = (scroll / height) // % of page scrolled
+        progress = (scroll / height) // % of screen scrolled
 
-    $(anims).each(function(i, a){
+    $(anims)
+      .filter(function(i, a){
+        var bs = a.bounds || [0, Number.MAX_VALUE]
+        return (bs[0] < progress) && (bs[1] >= progress)
+      })
+      .each(function(i, a){
+        var bs = a.bounds || [0, Number.MAX_VALUE]
+        var styles =
+          _.reduce(_.pairs(a.props), function(ps, p){
+            var name = p[0],
+                vs = p[1],
+                delta = bs[1] - bs[0]
+                coef =  (progress - bs[0]) / delta
+                current = (vs[1] - vs[0]) * coef + vs[0] + (vs[2] || '');
 
-      var styles =
-        _.reduce(_.pairs(a.props), function(ps, p){
-          var name = p[0],
-              vs = p[1],
-              current = (vs[1] - vs[0]) * progress + vs[0] + (vs[2] | '');
+            var x = {}
+            x[name] = current
+            return _.extend({}, ps, x)
+          }, {})
+        a.el.css(styles)
+      })
 
-            console.log("%o => %o", name, (vs[1] - vs[0]) * progress)
-
-          var x = {}
-          x[name] = current
-          return _.extend({}, ps, x)
-        }, {})
-
-      a.el.css(styles)
-
-    })
-
-  })
+  }, 15))
 })
