d39a4b76a18dfdc8cbc377d2f3f324661b267a73
jtournay <jtournay@linkedin.com>
2013-06-12 17:16:46 +0200

    Better scrolling for Ipad

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-cd /tmp;rm -Rf jto; git clone ~/Documents/perso/jto; cd jto; git checkout master;  rm -Rf ./*;cp -R ~/Documents/perso/jto/_site/* .; git add -u .; git commit -m "`date`"; git push;
+cd /tmp;rm -Rf jto; git clone ~/Documents/perso/jto; cd jto; git checkout master;  rm -Rf ./*;cp -R ~/Documents/perso/jto/_site/* .; git add -u .; git commit -m "`date`"; git push git@github.com:jto/jto.github.io.git master;
