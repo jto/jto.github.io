5419ab11e8a453fa383e5504c948addfb5a7b1b5
jtournay <jtournay@linkedin.com>
2013-06-17 18:08:00 +0200

    fix publish

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-cd /tmp;rm -Rf jto; git clone ~/Documents/perso/jto; cd jto; git checkout master;  rm -Rf ./*;cp -R ~/Documents/perso/jto/_site/* .; git add -u .; git commit -m "`date`"; git push -f git@github.com:jto/jto.github.io.git master;
+cd /tmp;rm -Rf jto; git clone ~/Documents/perso/jto; cd jto; git checkout master;  rm -Rf ./*;cp -R ~/Documents/perso/jto/_site/* .; git add -A .; git commit -m "`date`"; git push -f git@github.com:jto/jto.github.io.git master;
