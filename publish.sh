cdb47d03ee07fa200a39f3b84adc4cd8068aa939
jto <boudhevil@gmail.com>
2014-03-31 14:26:27 +0200

    blog design

--- publish.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-cd /tmp;rm -Rf jto; git clone ~/Documents/perso/jto; cd jto; git checkout master;  rm -Rf ./*;cp -R ~/Documents/perso/jto/_site/* .; git add -A .; git commit -m "`date`"; git push -f git@github.com:jto/jto.github.io.git master;
